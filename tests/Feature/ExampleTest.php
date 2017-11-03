70c0f565a98c66a726af9271dba01248468884d0
John <johnconley507@hotmail.com>
2017-11-03 15:03:59 -0400

    building tests to query data

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -25,5 +25,20 @@ class ExampleTest extends TestCase
 
         $this->get(route('api.products.index'))
              ->assertResponseOk();
+
+        array_map(function ($product) {
+            $this->seeJson($product->jsonSerialize());
+        }, $products->all());
+    }
+
+    public function testProductDescriptionsList() {
+        $product = factory(App\Product::class)->create();
+        $product->description()->saveMany(factory[\App\Description::class, 3]->make());
+
+        $this->get(route('api/products.description,index', ['products' => $products->id]))->assertResponseOk();
+
+        array_map(function ($description) {
+            $this->seeJson($description->jsonSerialize());
+        }, $product->descriptions()->all());
     }
 }
